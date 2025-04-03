99328e208a582bfec95f3676871c5e1b8a8bbad7
shantanurawade <103266755+shantanurawade@users.noreply.github.com>
2025-04-03 11:17:02 +0530

    Update index.js
    commented js doc

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import express, { json } from 'express';
 import dotenv from 'dotenv';
 import router from './routes/index.js';
 import { connectDb } from './db/db.js';
-import swaggerUi from 'swagger-ui-express';
-import jsonDoc from './swagger/swaagger.json' assert { type: 'json' };
+// import swaggerUi from 'swagger-ui-express';
+// import jsonDoc from './swagger/swaagger.json' assert { type: 'json' };
 
 dotenv.config({
     path: './.env'
@@ -13,8 +13,8 @@ const app = express();
 
 connectDb().then(() => {
     try {
-        app.listen(process.env.PORT, (req, res) => {
-            console.log("Application is listening to - ", process.env.PORT);
+        app.listen(8000, (req, res) => {
+            console.log("Application is listening to - ", 8000);
             console.log('====================================');
         })
     }
@@ -23,6 +23,6 @@ connectDb().then(() => {
     }
 });
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(jsonDoc));
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(jsonDoc));
 app.use(json());
 app.use(`/api/`, router());
