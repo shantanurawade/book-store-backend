a2db6f181015ed5bbc3ad11d140c186d54768e76
shantanurawade <103266755+shantanurawade@users.noreply.github.com>
2025-04-03 11:46:54 +0530

    Update db.js
    db name added

--- src/db/db.js
+++ src/db/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDb = async () => {
     try {
-        await mongoose.connect(`mongodb+srv://rawadeshantanu:Pass%40123@cluster0.i1ora.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`).then(() => {
+        await mongoose.connect(`mongodb+srv://rawadeshantanu:Pass%40123@cluster0.i1ora.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/mydb`).then(() => {
             console.log('====================================');
             console.log('db connected...');
         });
