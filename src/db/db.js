10478cac21ce64260324e8df996b7dbb34c01da4
shantanurawade <103266755+shantanurawade@users.noreply.github.com>
2025-04-03 11:17:49 +0530

    Update db.js
    changed mongodb server due to dotenv

--- src/db/db.js
+++ src/db/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDb = async () => {
     try {
-        await mongoose.connect(`${process.env.MONGO_URI}/${process.env.DB_Name}`).then(() => {
+        await mongoose.connect(`mongodb+srv://rawadeshantanu:Pass%40123@cluster0.i1ora.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`).then(() => {
             console.log('====================================');
             console.log('db connected...');
         });
